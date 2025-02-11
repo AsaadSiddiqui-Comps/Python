ecd1d8c82ef04720efe51e71527f7cc9abfdd43b
Asaad Siddiqui <asaad.241258.co@mhssce.ac.in>
2025-02-11 21:09:18 +0530

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,20 @@
+# NAME: Siddiqui Mohammed Asaad Shakir
+#Roll no: 241258
+#Branch:computer engineering   Batch: B3
+# PROGRAM NO 2 Mudule 1
+# Python Program to Developing Conversion Utilities
+
+# Currency Convert:- INR to USD
+inr = float(input("Enter the amount in INR: "))
+usd = inr * 0.014
+print(f"The equivalent amount in USD is: {usd}")
+
+# Temperature Convert:- Celsius to Fahrenheit
+celsius = float(input("Enter the temperature in celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {fahrenheit}°F")
+
+# Inches to feet conversion
+inches = float(input("Enter the length in inches: "))
+feet = inches / 12
+print(f"{inches} inches is equal to {feet} feet.")
---
+++ exp3.py
@@ -0,0 +1,13 @@
+# NAME: Siddiqui Mohammed Asaad Shakir
+#Roll no: 241258
+#Branch:computer engineering   Batch: B3
+# PROGRAM NO 3 Mudule 1
+# Python Program to Calculate Simple Interest
+
+principle = float(input("Enter the Principle amount: "))
+rate = float(input("Enter the rate of interest per annum: "))
+time = float(input("Enter the time period: "))
+
+interest = (principle * rate * time) / 100
+
+print("The Interest is:", interest)
