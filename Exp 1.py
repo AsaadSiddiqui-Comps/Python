317c789ab19448f0677f8dc5bb698410983ac2f8
Asaad Siddiqui <asaad.241258.co@mhssce.ac.in>
2025-02-12 19:47:55 +0530

    Create Exp 1.py

---
+++ Exp 1.py
@@ -0,0 +1,22 @@
+# NAME: Siddiqui Mohammed Asaad Shakir
+# Roll no: 241258
+# Branch:computer engineering   Batch: B3
+# PROGRAM NO 1 Mudule 1
+# Calculating Areas of Geometric Figures
+
+# Calculating the Area of Circle
+radius = float(input("Enter the radius of the circle: "))
+circle_area = 3.142 * radius * radius
+print(f"The area of the circle is: {circle_area}")
+
+# Calculating the Area of Rectangle
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+rectangle_area = length * breadth
+print(f"The area of the rectangle is: {rectangle_area}")
+
+# Calculating the Area of Triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+triangle_area = 0.5 * base * height
+print(f"The area of the triangle is: {triangle_area}")
